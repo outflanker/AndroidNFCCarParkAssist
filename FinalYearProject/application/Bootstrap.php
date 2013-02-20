75a29f962852d9ca29f01918cb6651dbebcea7a4
Siddharth Sinha <sidzone.siddharth@gmail.com>
2013-02-20 22:58:07 +0530

    modified bootstrap.php

--- FinalYearProject/application/Bootstrap.php
+++ FinalYearProject/application/Bootstrap.php
@@ -1,8 +1,11 @@
 <?php
-
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+        protected function _initRestRoute()
+        {
+            $this->bootstrap('frontController');
+            $frontController = Zend_Controller_Front::getInstance();
+            $restRoute = new Zend_Rest_Route($frontController);
+            $frontController->getRouter()->addRoute('default', $restRoute);
+        }
 }
-
