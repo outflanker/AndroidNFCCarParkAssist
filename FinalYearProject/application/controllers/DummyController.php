9ec0886d7f4d3eb18e9e6c5c64e16f6dc0ead900
NITIN BHAT <nitbhat@yahoo.co.in>
2013-02-21 19:32:47 +0530

    changed dummycontroller from get to put

--- FinalYearProject/application/controllers/DummyController.php
+++ FinalYearProject/application/controllers/DummyController.php
@@ -5,25 +5,24 @@ class DummyController extends Zend_Controller_Action
 
     public function init()
     {
-	    print "Inside Init of Dummy Controller";
+	    print "Inside Init of Dummy Controller kaa bee";
 		/* Initialize action controller here */
     }
+
 
     public function indexAction()
     {
         // action body
     }
 	
-	public function getAction()
-	{
-		$response = $this->getResponse();
-        //$uuid = $this->_getParam ('id');
-        //$password = $this->_getParam('password');
-        $incoming = file_get_contents("php://input");
+	public function putAction()
+	{	
+		$incoming = file_get_contents("php://input");
         $json = json_decode($incoming,true);
         $password =  $json['password'];
         //$aws = AwsConnect::connectDynamoDB();
         
+		$response=$this->getResponse();
 		$response->setHttpResponseCode(400);
         $why["error"]="Could not find email";
         $why["code"]="27";
