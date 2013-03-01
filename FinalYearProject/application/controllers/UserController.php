262b67bcb5661f6e60ca743afdf6443d594d1192
Siddharth Sinha <sidzone.siddharth@gmail.com>
2013-03-01 16:35:12 +0530

    added phtml files

--- FinalYearProject/application/controllers/UserController.php
+++ FinalYearProject/application/controllers/UserController.php
@@ -6,7 +6,29 @@ class UserController extends Zend_Rest_Controller
         
     }
 
-    public function getAction() {
+    public function getAction() 
+    {
+        $response=$this->getResponse();
+                
+        $con = mysql_connect("localhost","appadmin","apppwd");
+        if (!$con)
+        {
+              print "Error";
+              die('Could not connect: ' . mysql_error());
+        }
+        else
+        {
+                mysql_select_db("parkingdatabase", $con);
+                $query="SELECT * FROM ACCOUNTS";
+                $res=mysql_query($query);
+                $jsonreturn=null;
+                while($row = mysql_fetch_array($res))
+                {
+                    $jsonreturn[$row['USER']]=$row['UPASSWORD'];
+                 }
+                 return $response->appendBody(json_encode($jsonreturn));
+         }
+        mysql_close($con);
         
     }
 
@@ -18,11 +40,29 @@ class UserController extends Zend_Rest_Controller
         
     }
 
-    public function postAction() {
-        
+    public function postAction()
+    {
+        $incoming = file_get_contents("php://input");
+        $json = json_decode($incoming,true);
+        $uname =  $json['user'];
+        $pass = $json['password'];
+        $con = mysql_connect("localhost","appadmin","apppwd");
+        if (!$con)
+        {
+              print "Error";
+              die('Could not connect: ' . mysql_error());
+        }
+        else
+        {
+                mysql_select_db("parkingdatabase", $con);
+                $query="INSERT INTO ACCOUNTS (USER,UPASSWORD) VALUES ('".$uname."','".$pass."')";
+                $res=mysql_query($query);
+         }
+        mysql_close($con);
     }
 
-    public function putAction() {
+    public function putAction() 
+    {
         
     }    
 }
