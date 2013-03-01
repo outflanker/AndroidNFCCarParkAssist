3d154a8daea3a9739b097aeaade2941f03dca4ba
Siddharth Sinha <sidzone.siddharth@gmail.com>
2013-03-01 13:18:08 +0530

    changed bootstrap to include new controllers

--- FinalYearProject/application/Bootstrap.php
+++ FinalYearProject/application/Bootstrap.php
@@ -5,7 +5,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         {
             $this->bootstrap('frontController');
 			$frontController = Zend_Controller_Front::getInstance();
-			$restRoute = new Zend_Rest_Route($frontController,array(), array('default'=>array('Dummy')));
+			$restRoute = new Zend_Rest_Route($frontController,array(), array('default'=>array('Dummy','Layer','Layout','Slot','User')));
 			$frontController->getRouter()->addRoute('default', $restRoute);
         }
 }
